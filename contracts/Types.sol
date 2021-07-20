1930d2b23a68b8f50db59a6cbfe909f603ae1e71
Michael Coon <mdcoon1@yahoo.com>
2021-07-20 17:41:35 -0400

    Update Types.sol
    Updated comment

--- contracts/Types.sol
+++ contracts/Types.sol
@@ -28,7 +28,7 @@ library Types {
         //the token we are taking as fee
         IERC20 feeToken;
 
-        //eth price of fee token in whole units
+        //eth price of fee token in whole ETH units
         uint feeTokenETHPrice;
 
         //an estimate of gas to use to fill the order
